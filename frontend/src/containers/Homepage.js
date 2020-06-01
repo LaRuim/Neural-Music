564366dd82d3bf3b5d247799ee98db82cae3b613
Murali <mur.kri14@gmail.com>
2020-06-01 09:41:04 +0530

    backing up

--- frontend/src/containers/Homepage.js
+++ frontend/src/containers/Homepage.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import Sidebar from "react-sidebar";
 import 'bootstrap/dist/css/bootstrap.min.css';
 //import './App.css';
 
@@ -9,5 +10,51 @@ const HomePage = () => {
         </div>
     )
 }
+ 
+const mql = window.matchMedia(`(min-width: 800px)`);
+ 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sidebarDocked: mql.matches,
+      sidebarOpen: false
+    };
+ 
+    this.mediaQueryChanged = this.mediaQueryChanged.bind(this);
+    this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
+  }
+ 
+  componentWillMount() {
+    mql.addListener(this.mediaQueryChanged);
+  }
+ 
+  componentWillUnmount() {
+    this.state.mql.removeListener(this.mediaQueryChanged);
+  }
+ 
+  onSetSidebarOpen(open) {
+    this.setState({ sidebarOpen: open });
+  }
+ 
+  mediaQueryChanged() {
+    this.setState({ sidebarDocked: mql.matches, sidebarOpen: false });
+  }
+ 
+  render() {
+    return (
+      <Sidebar
+        sidebar={<b>Sidebar content</b>}
+        open={this.state.sidebarOpen}
+        docked={this.state.sidebarDocked}
+        onSetOpen={this.onSetSidebarOpen}
+      >
+        <b>Main content</b>
+      </Sidebar>
+    );
+  }
+}
+ 
+export default App;
 
 export default HomePage
